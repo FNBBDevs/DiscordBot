a61e4f7bf2611860c91779642bd9ac92285e0d3c
Nolan Gregory <65730528+nulzo@users.noreply.github.com>
2023-02-15 00:24:45 -0600

    Added comments for readability.

---
+++ COMMANDS.py
@@ -0,0 +1,5 @@
+# Example file for which commands can exist. Similarly, they can exist on the database in JSON format.
+
+COMMANDS = {
+    "!ping": "Pong!"
+}
--- FortniteBallsBot.py
+++ FortniteBallsBot.py
@@ -1,17 +1,41 @@
+#
+# DISCORD BOT
+
+# Authors: Nolan Gregory, ... , ...
+# Version: 0.69
+# Intent: This multifaceted bot is to promote user activity within
+#         servers that it is hosted in.
+#
+
+# --------------------------- IMPORT STATEMENTS -------------------------------
+
 import Config
 import re
-import pymongo
-from pymongo import MongoClient
-from discord.ext import commands
 import discord
+import COMMANDS
+import random
+
+# ------------------------- VARIABLE INSTANTIATION ----------------------------
+
+COMMAND_PREFIX = '!'
+BOT_NAME = "Fortnite Balls Bot"
+intents = discord.Intents.default()
+intents.message_content = True
+commands = COMMANDS.COMMANDS
+
+# --------------------------- CLASS DECLARATION --------------------------------
 
 
 class FortniteBot(discord.Client):
 
+    # !!! PREDEFINED METHOD NAME... DO NOT CHANGE !!!
     async def on_ready(self):
+        """The on_ready method will wait until the bot is hosted and then run."""
         print(f"logged in as {self.user}")
 
+    # !!! PREDEFINED METHOD NAME... DO NOT CHANGE !!!
     async def on_message(self, message):
+        """The on_message method will constantly scan any newly received messages. Very useful."""
         process = re.compile('[^a-zA-Z]')
         # process the message into sanitised format
         processed = str(message.content).lower().strip()
@@ -19,7 +43,7 @@ class FortniteBot(discord.Client):
         print(f"Message from {message.author}: {message.content}")
 
         # --- start of checks ---
-        if message.author == client.user:
+        if message.author == bot.user:
             return
 
         # --- Ethan's Method for getting fortnite balls ---
@@ -35,9 +59,20 @@ class FortniteBot(discord.Client):
         if "bruhshell" in process.sub('', str(message.content)).lower():
             await message.channel.send("! ! ! BRUH SHELL IS A CRYPTO-MINING SPYWARE ! ! !")
 
+        # Let's assume we want to parse bot commands - we could simply do something like what I illustrate below.
+        if message.content[0] == COMMAND_PREFIX:
+            # Here we could go ahead and search through a command base - I will just code one in as an example.
+            if not message.content in commands:
+                await message.channel.send(f"Hey, <@{message.author.id}>, that command was not found, bud!")
+            else:
+                await message.channel.send(commands[message.content])
 
-# setting up everything
-intents = discord.Intents.default()
-intents.message_content = True
-client = FortniteBot(intents=intents)
-client.run(Config.TOKEN)
+        # Lastly, for fun, let's assume that the user pings the bot (bad idea)
+        if str(message.content).__contains__("<@1075154837609656350>"):
+            await message.channel.send(f"Hey, <@{message.author.id}>, don't ping me, jackass!")
+
+
+# -------------------- CREATE THE BOT OBJECT AND BOOT UP ------------------------
+
+bot = FortniteBot(intents=intents)
+bot.run(Config.TOKEN)

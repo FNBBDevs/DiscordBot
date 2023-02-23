eb359df474b244bc0577b55339e2abc4774f001d
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-02-22 22:50:56 -0600

    py dev cp dev be like

--- BOT/_commands/contains.py
+++ BOT/_commands/contains.py
@@ -11,6 +11,8 @@ class Contains:
             'lol':"https://i.imgflip.com/7b8363.gif",
             'bruhshell':"! ! ! BRUH SHELL IS A CRYPTO-MINING SPYWARE ! ! !",
             'etchris': "https://soundcloud.com/etchris?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+            'c++': "C++ Dev be like:\nhttps://tenor.com/view/legs-anothersh0tatlife-gif-18722244",
+            'python': "Python Dev be like :P\nhttps://tenor.com/view/gigachad-chad-gif-20773266",
             #'give me some dat cardi':"https://media.discordapp.net/attachments/1069835760859107371/1076370583807328256/image0.png"
 
         }
@@ -23,7 +25,7 @@ class Contains:
         :param debug     : to print debug messages
         :param out       : the resulting responses the bot should return
         """
-        filter = re.compile('[^a-zA-Z]')
+        filter = re.compile('[^a-zA-Z+]')
         message_in = filter.sub('', message_in)
         out = []
 

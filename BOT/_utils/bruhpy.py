46ef319470c79ecdf5ef46b198a6747415b0c06c
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-02-26 19:59:20 -0600

    comments

--- BOT/_utils/bruhpy.py
+++ BOT/_utils/bruhpy.py
@@ -6,6 +6,10 @@ from io import StringIO
 
 @contextlib.contextmanager
 def stdoutIO(stdout=None):
+    """
+    Function to route stdout to a new stdout and
+    capture the output
+    """
     old = sys.stdout
     if stdout is None:
         stdout = StringIO()
@@ -14,6 +18,13 @@ def stdoutIO(stdout=None):
     sys.stdout = old
 
 def execute_processed_command(program, results, debug):
+    """
+    Function for executing the code and capturing any output to 
+    stdout
+    :param program: the code to execute
+    :param results: multiprocessing Manager Dict to store the stdout to
+    :param debug  : to show debug messages
+    """
     if debug: print(f"\nEPC called with\n{program}\n")
     with stdoutIO() as s:
         try:
@@ -24,6 +35,9 @@ def execute_processed_command(program, results, debug):
             results['POST'] = ('ERROR', "-[ERROR]: " + str(exception))
 
 class BruhPy:
+    """
+    Class responsible for processing of incoming python code from the /bruh command
+    """
     def __init__(self, debug=False):
         self._debug     = debug
         self._responses = [] 
@@ -31,14 +45,24 @@ class BruhPy:
         self._results   = self._manager.dict()
     
     def run(self, arg, argvs):
+        """
+        Parse, prepare and execute the code passed in
+        :param arg  : second word in the command
+        :param argvs: every other word in the command
+        """
         if arg == '-s':
             pre_process = f"{' '.join(argvs) if argvs else ''}".replace('#', '\n').replace('\\t', '\t').replace("“", "\"").replace("”", "\"")
             self._responses.append(('PY', pre_process))
         else: pre_process = f"{arg + ' ' + (' '.join(argvs) if argvs else '')}".replace('#', '\n').replace('\\t', '\t').replace("“", "\"").replace("”", "\"")
+
+        # Execute the code 
         process = multiprocessing.Process(target=execute_processed_command, args=(pre_process, self._results, self._debug))
         process.start()
+
+        # sleep while code is running
         time.sleep(2)
 
+        # timeout after the two seconds
         if process.is_alive():
             process.terminate()
             self._responses.append(('ERROR', '-[ERROR]: valid runtime exceeded!'))

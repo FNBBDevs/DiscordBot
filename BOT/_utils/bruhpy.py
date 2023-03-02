afae627dafe57db804ff36bf4a2d884372ede0a6
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-03-01 20:12:16 -0600

    marcus

--- BOT/_utils/bruhpy.py
+++ BOT/_utils/bruhpy.py
@@ -1,11 +1,10 @@
 import re
 import sys
 import time
-import random
 import contextlib
-import python_weather
 import multiprocessing
 from io import StringIO
+from _utils.marcus import Marcus
 from _utils.restrictions import BRUHPY_RESTRICTIONS
 
 
@@ -37,7 +36,7 @@ def execute_processed_command(program, results, debug, pvn):
         try:
             exec(f"""\n{program}\n""")
             if s.getvalue() != '':
-                results['POST'] = ('NORMAL', '[OUTPUT]\n'+s.getvalue())
+                results[pvn] = ('NORMAL', '[OUTPUT]\n'+s.getvalue())
             else:
                 results[pvn] = ('INFO', '[INFO]: no output produced')
         except Exception as exception:
@@ -69,12 +68,13 @@ class BruhPy:
     """
 
     def __init__(self, debug=False, post_val_name='POST'):
-        self._debug = debug
-        self._responses = []
-        self._manager = multiprocessing.Manager()
-        self._results = self._manager.dict()
-        self._restictions = BRUHPY_RESTRICTIONS + ['exec', 'eval']
+        self._debug         = debug
+        self._responses     = []
+        self._manager       = multiprocessing.Manager()
+        self._results       = self._manager.dict()
+        self._restictions   = BRUHPY_RESTRICTIONS + ['exec', 'eval']
         self._post_val_name = post_val_name
+        self.marcus         = Marcus()
 
     def run(self, arg, argvs):
         """
@@ -82,7 +82,6 @@ class BruhPy:
         :param arg  : second word in the command
         :param argvs: every other word in the command
         """
-        print(arg, argvs)
         if arg == '-s':
             pre_process = f"{' '.join(argvs) if argvs else ''}".replace('#', '\n').replace(
                 '\\t', '\t').replace("“", "\"").replace("”", "\"").replace("\\\\", "\\")
@@ -90,13 +89,10 @@ class BruhPy:
             pre_process = f"{arg + ' ' + (' '.join(argvs) if argvs else '')}".replace(
                 '#', '\n').replace('\\t', '\t').replace("“", "\"").replace("”", "\"").replace("\\\\", "\\")
 
-        code_check = self._check(pre_process)
+        code_check = self.marcus.erm__hey_marcus__can_you_check_this_code_out(pre_process)
 
         if not code_check:
-            self._responses.append(
-                ("ERROR", "-[ERROR]: code did not pass preliminary inspection"))
-            self._responses.append(
-                ("INFO", "[INFO]: code did not execute, no output produced"))
+            self._responses += [("ERROR", "-[ERROR]: code did not pass preliminary inspection"), ("INFO", "[INFO]: code did not execute, no output produced")]
             return self._responses
 
         # Execute the code
@@ -116,84 +112,3 @@ class BruhPy:
             self._responses.append(self._results[self._post_val_name])
 
         return self._responses
-
-    def _check(self, program):
-        checks = [
-            r""".*(eval[(].*[)]).*""",
-            r""".*(exec[(].*[)]).*""",
-            r""".*(import .*).*""",
-            r""".*(__import__[(].*[)]).*""",
-            r""".*(globals[(][)]).*""",
-            r""".*(getattr[(].*[)]).*""",
-            r""".*(open[(].*[)]).*""",
-            r""".*(while True:).*""",
-        ]
-        anti_checks = [
-            r"""(.*(".*eval.*[(].*[)].*").*)|(.*('.*eval.*[(].*[)].*').*)""",
-            r"""(.*(".*exec.*[(].*[)].*").*)|(.*('.*exec.*[(].*[)].*').*)""",
-            r"""(.*(".*import .*").*)|(.*('.*import .*').*)""",
-            r"""(.*(".*__import__[(].*[)].*").*)|(.*('__import__[(].*[)].*').*)""",
-            r"""(.*(".*globals[(][)].*").*)|(.*('.*globals[(][)].*').*)""",
-            r"""(.*(".*getattr[(].*[)].*").*)|(.*('.*getattr[(].*[)].*').*)""",
-            r"""(.*(".*open[(].*[)].*").*)|(.*('.*open[(].*[)].*').*)""",
-            r"""(.*(".*while True:.*").*)|(.*('.*while True:.*').*)""",
-        ]
-        no_antis = [
-            r"""(.*(f".*{.*eval.*[(].*[)].*}.*").*)|(.*(f'.*{.*eval.*[(].*[)].*}.*').*)""",
-            r"""(.*(f".*{.*exec.*[(].*[)].*}.*").*)|(.*(f'.*{.*exec.*[(].*[)].*}.*').*)""",
-            r"""(.*(f".*{.*import .*}.*").*)|(.*(f'.*{.*import .*}.*').*)""",
-            r"""(.*(f".*{.*__import__[(].*[)].*}.*").*)|(.*(f'.*{.*__import__[(].*[)].*}.*').*)""",
-            r"""(.*(f".*{.*globals[(][)].*}.*").*)|(.*(f'.*{.*globals[(][)].*}.*').*)""",
-            r"""(.*(f".*{.*getattr[(].*[)].*}.*").*)|(.*(f'.*{.*getattr[(].*[)].*}.*').*)""",
-            r"""(.*(f".*{.*open[(].*[)].*}.*").*)|(.*(f'.*{.*open[(].*[)].*}.*').*)""",
-            r"""(.*(f".*{.*while True:.*}.*").*)|(.*(f'.*{.*while True:.*}.*').*)""",
-        ]
-        no_antis_antis = [
-            r"""(.*(f".*{.*'.*eval.*[(].*[)].*'.*}.*").*)|(.*(f'.*{.*".*eval.*[(].*[)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*exec.*[(].*[)].*'.*}.*").*)|(.*(f'.*{.*".*exec.*[(].*[)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*import .*'.*}.*").*)|(.*(f'.*{.*".*import .*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*__import__[(].*[)].*'.*}.*").*)|(.*(f'.*{.*".*__import__[(].*[)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*globals[(][)].*'.*}.*").*)|(.*(f'.*{.*".*globals[(][)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*getattr[(].*[)].*'.*}.*").*)|(.*(f'.*{.*".*getattr[(].*[)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*open[(].*[)].*'.*}.*").*)|(.*(f'.*{.*".*open[(].*[)].*".*}.*').*)""",
-            r"""(.*(f".*{.*'.*while True:.*'.*}.*").*)|(.*(f'.*{.*".*while True:.*".*}.*').*)""",
-        ]
-            
-        hits = []
-        flag = False
-        lines = program.split("\n")
-        for line in lines:
-            if flag:break
-            line = line.split(";")
-            for hidden_line in line:
-                hidden_line = hidden_line.replace('"""', '"')
-                for check, anti_check in list(zip(checks, anti_checks)):
-                    if (s1 := re.search(check, hidden_line)) and not (s2 := re.search(anti_check, hidden_line)):
-                        hits.append(s1)
-                        hits.append(s2)
-                        flag = True
-                        break
-                for restriction in self._restictions:
-                    check_1 = r"""(.*=.*"""+restriction+r""".*)"""
-                    check_2 = r"""(.*[(]"""+restriction+r"""[)].*)"""
-                    anti_check_1 = r"""(.*=.*\""""+restriction+r""".*")"""
-                    anti_check_1_2 = r"""(.*=.*'"""+restriction+r""".*')"""
-                    anti_check_2 = r"""(.*[(]\""""+restriction+r"""[)].*")"""
-                    anti_check_2_2 = r"""(.*[(]'"""+restriction+r"""[)].*')"""
-                    if re.search(check_1, hidden_line) or re.search(check_2, hidden_line):
-                        if not (re.search(anti_check_1, hidden_line) or re.search(anti_check_1_2, hidden_line) or re.search(anti_check_2, hidden_line) or re.search(anti_check_2_2, hidden_line)):
-                            break
-                        else:
-                            hits.append(check_1)
-                            hits.append(check_2)
-                            flag = True
-                            break
-                for check, anti_check in list(zip(no_antis, no_antis_antis)):
-                    if re.search(check, hidden_line) and not re.search(anti_check, hidden_line):
-                        hits.append(check)
-                        hits.append(anti_check)
-                        flag = True
-                        break
-        print(hits)
-        #return not flag
-        return True if input("allow?: ").strip().lower() == "y" else False

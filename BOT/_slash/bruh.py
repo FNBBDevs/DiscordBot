c3464a3874828c58d13712932fc01741a4b730e1
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-03-04 21:06:13 -0600

    refactor some more

--- BOT/_slash/bruh.py
+++ BOT/_slash/bruh.py
@@ -6,12 +6,18 @@ from _utils.bruhpy import BruhPy
 class bruh:
     def __init__(self, tree, guild):
 
-        self._weather_options = ['current', 'forecast']
-        self._bruhpy_options  = ['show code', 'don\'t show code']
+        self._weather_options = [('current', '🌡️'), ('forecast', '⌚')]
+        self._bruhpy_options  = [('show code', '✅'), ('don\'t show code', '❌')]
         self._valid_commands  = {
             'help': 'The **help** command provides information about each command, the arguments they take in, and the expected response. You provide a `command`, and the bot responds with information about that command.',
             'weather': 'The **weather** command allows you get the current temperature or forecast for a given city. You provide whether you want the `current` temperature or `forecast`, and then provvide a `city`. The bot reponds with the corresponding information about that city.',
             'bruhpy': 'The **bruhpy** command allows you get execute python code with the bot. You provide code the bot, and if it passes inspection, the code is executed and the bot displays the execution ouput.\nYour python code must follow a certain format where newlines in the program are replaced with `#` and tabs are still `\\t` If you want to use a newline within a string, use the standard `\\n`. An example program might look like this:\n```for i in range(10):#\\tprint("Hello, \\n world!")```'
+        }
+
+        self._command_information = {
+            'help': ("Learn about what the commands do!","❔"),
+            'weather': ("Get the weather or forecast for a City!", "🌤️"),
+            'bruhpy': ("Execute Python Code!", "🐍")
         }
 
 
@@ -39,25 +45,21 @@ class bruh:
 
             await interaction.response.defer()
 
+            options = []
+
+            for option in self._command_information:
+                options.append(discord.SelectOption(
+                    label=option.capitalize(),
+                    emoji=self._command_information[option][1],
+                    description=self._command_information[option][0],
+                    value=option[0]
+                ))
+
             initial_select = Select(
                 placeholder="What command would you like to execute?",
-                options=[
-                discord.SelectOption(
-                    label="Help",
-                    emoji="❔",
-                    description="Learn about what the commands do!",
-                    value="h"),
-                discord.SelectOption(
-                    label="Weather",
-                    emoji="🌤️",
-                    description="Get the weather or forecast for a City!",
-                    value="w"),
-                discord.SelectOption(
-                    label="BruhPy",
-                    emoji="🐍",
-                    description="Execute Python Code!",
-                    value="b"),
-            ])
+                options=options
+            )
+
             initial_select.callback = callback
             view = View()
             view.add_item(initial_select)
@@ -67,18 +69,17 @@ class bruh:
     def get_help_options(self):
 
         async def callback(interaction):
-
             help_info = self._valid_commands[help_select.values[0]]
             await interaction.response.edit_message(content=help_info, view=None)
 
         options = []
-        for option in self._valid_commands:
+        for option in self._command_information:
             options.append(
-                discord.SelectOption(label=option.capitalize(), emoji="❔", value=option)
+                discord.SelectOption(label=option.capitalize(), emoji=self._command_information[option][1], value=option)
             )
         
         help_select = Select(
-            placeholder="What command?",
+            placeholder="What command to get info about?",
             options=options
         )
 
@@ -101,7 +102,7 @@ class bruh:
         options = []
         for option in self._weather_options:
             options.append(
-                discord.SelectOption(label=option.capitalize(), emoji="🌤️", value=option)
+                discord.SelectOption(label=option[0].capitalize(), emoji=option[1], value=option[0])
             )
         weather_select = Select(
             placeholder="Do you want the current Temperature or a Forecast?",
@@ -127,7 +128,7 @@ class bruh:
         options = []
         for option in self._bruhpy_options:
             options.append(
-                discord.SelectOption(label=option.capitalize(), emoji="🐍", value=option)
+                discord.SelectOption(label=option[0].capitalize(), emoji=option[1], value=option[0])
             )
         bruhpy_select = Select(
             placeholder="Do you want to display the code?",

776a7787a2bbd4035ec8a2e0401a322de4b164fd
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-03-04 20:40:36 -0600

    refactored the bruh command to make use of selection UI and modal UI

---
+++ BOT/_utils/modals.py
@@ -0,0 +1,49 @@
+"""DEFINE YOUR MODALS HERE"""
+
+
+import discord
+from discord import ui as UI
+from discord.ui import Modal
+from _utils.weather import get_weather as Weather
+from _utils.bruhpy import BruhPy
+
+class UserInputModal(Modal):
+    def __init__(self, prompt, short_or_long, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if short_or_long == 'short':
+            self.add_item(UI.TextInput(label=prompt, style=discord.TextStyle.short))
+        else:
+            self.add_item(UI.TextInput(label=prompt, style=discord.TextStyle.long))
+
+
+class WeatherModal(Modal):
+    def __init__(self, typE, prompt, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_item(UI.TextInput(label=prompt, style=discord.TextStyle.short))
+        self._typE = typE
+
+    async def on_submit(self, interaction: discord.Interaction):
+        weather = await Weather(self.children[0].value, self._typE)
+        await interaction.response.edit_message(content=weather, view=None)
+
+class BruhPyModal(Modal):
+    def __init__(self, show_code, prompt, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_item(UI.TextInput(label=prompt, style=discord.TextStyle.long))
+        self._tags = {
+            'ERROR':  'diff',
+            'NORMAL': '',
+            'PY':     'py',
+            'INFO':   'fix', 
+        }
+        self._show_code = show_code
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        orignal_response = await interaction.original_response()
+        response = ''
+        program = self.children[0].value.split(' ')
+        for res in BruhPy(debug=False).run("-s" if self._show_code else program[0], program if self._show_code else program[1:]):
+            response += f"```{self._tags[res[0]]}\n{res[1]}\n```\n"
+        await orignal_response.edit(content=response, view=None)
---
+++ BOT/_utils/weather.py
@@ -0,0 +1,28 @@
+import python_weather
+
+
+async def get_weather(city, typE="current"):
+    async with python_weather.Client(format="F") as client:
+        response = await client.get(city)
+        if typE == "current":
+            return f"```\nThe current temperature in {city} is {response.current.temperature}°F {response.current.type!r}\n```"
+        elif typE == "forecast":
+            forecast_response = ''
+            for i, forecast in enumerate(response.forecasts):
+                if i == 1:
+                    break
+                date = f"Date: {forecast.date}"
+                sunrise = f"Sunrise: {forecast.astronomy.sun_rise}"
+                sunset = f"Sunset: {forecast.astronomy.sun_set}"
+                forecast_response += f"{date:<25s}\n{sunrise:<25s}{sunset:<24s}\n"
+                for hourly in forecast.hourly:
+                    time_span = f"{str(hourly.time.hour).rjust(2, '0')}:{str(hourly.time.minute).ljust(2, '0')}    {str(hourly.temperature).rjust(3, ' ')}°F"
+                    info = f"{str(hourly.description).ljust(14, ' ')}{hourly.type!r} "
+                    if hourly.description in ["Mist", "Partly cloudy"]:
+                        forecast_response += f"{time_span:25s}{info:<28s}\n"
+                    else:
+                        forecast_response += f"{time_span:25s}{info:<29s}\n"
+            return f"```\n{forecast_response}\n```"
+        else:
+            return "**No Response**"
+

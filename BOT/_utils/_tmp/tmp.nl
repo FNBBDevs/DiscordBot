23457602c2c24923fac32ff2e032a89ab8f475fe
Ethan Christensen <ethanlchristensen@outlook.com>
2023-05-21 15:39:34 -0500

    Nolang support and better colored output messages for bruhpy and nolang

---
+++ BOT/_utils/_tmp/tmp.nl
@@ -0,0 +1,4 @@
+no x = 0
+while x < 10
+	nolout('cock')
+	x = x  + 1
--- BOT/_utils/convert.py
+++ BOT/_utils/convert.py
@@ -1,5 +1,6 @@
-with open('_tmp/tmp.py', 'r') as f:
-    lines = [str(repr(line).replace('\\n', '#').replace('    ', '\\t')) for line in f.readlines()]
+nl_sep = input('Newline seperator: ')
+with open('_tmp/tmp.py', 'r', encoding='utf-8') as f:
+    lines = [str(repr(line).replace('\\n', nl_sep).replace('    ', '\\t')) for line in f.readlines()]
 out = ''
 for line in lines: out+=line[1:-1]
 print(out)
